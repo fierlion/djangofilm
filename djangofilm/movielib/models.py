e9cee423fa07786b1ae737dce923456ce6cb3654
fierlion <fierlion@gmail.com>
2015-11-21 11:30:43 -0800

    added models

--- djangofilm/movielib/models.py
+++ djangofilm/movielib/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    movielength = models.IntegerField(default=0)
+    theater = models.ForeignKey(Theater, null=True)
+
+class Theater(models.Model):
+    name = models.CharField(max_length=200)
+    chambers = models.IntegerField(default=0)
+
